7a63f5e0662d7fcaf20adf23142670ad3e387dc2
Yuxia Xu <hxu@marinsoftware.com>
2017-06-19 23:22:19 -0700

    Update with basic

---
+++ src/main/java/chengzi/leetcode/Test.java
@@ -0,0 +1,10 @@
+package chengzi.leetcode;
+
+/**
+ * Created by seasons on 6/19/17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
